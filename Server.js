39e10d9c80feed037ecda923398c697091898911
Md Monauwarul Islam <121303308+mdmonauwarulislam@users.noreply.github.com>
2024-06-29 19:36:21 +0530

    port chnge

--- Server.js
+++ Server.js
@@ -8,7 +8,7 @@ const ToDoRoute = require('./Routes/ToDoRoute');
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(cors());
