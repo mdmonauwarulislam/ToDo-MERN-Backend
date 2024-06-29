0da0c87bcf5382bf6e44968cd743ad1efabfebfa
Md Monauwarul Islam <121303308+mdmonauwarulislam@users.noreply.github.com>
2024-06-29 18:50:47 +0530

    Fix path for ToDoRoute

--- Server.js
+++ Server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const ToDoRoute = require('./routes/ToDoRoute');
 require("dotenv").config();
-const router = require('./routes/ToDoRoute');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URL)
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.log('MongoDB connection error:', err));
 
-app.use(router); 
+app.use(ToDoRoute); 
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
